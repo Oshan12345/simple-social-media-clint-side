8b138708dde8a6b7f6cf7bbf3b431b81188341c1
Oshan12345 <bsagor99@gmail.com>
2021-05-21 23:33:56 +0600

    added post delete function to userProfile page

--- src/Components/MyPostCard.js
+++ src/Components/MyPostCard.js
@@ -1,9 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 
 const MyPostsCard = ({ post }) => {
-  const { body, photo, title } = post;
+  const [isHidePost, setIsHidePost] = useState(false);
+  const { body, photo, title, _id } = post;
+  const deletePost = (postId) => {
+    const { token } = JSON.parse(localStorage.getItem("instragram-jwt"));
+
+    fetch(
+      `https://mighty-springs-16344.herokuapp.com/delete-my-post/${postId}`,
+      {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setIsHidePost(true);
+      });
+  };
+
   return (
-    <div className="col">
+    <div className={`col position-relative  ${isHidePost && "d-none"}`}>
+      <div>
+        <i
+          className="bi bi-trash-fill delete-btn"
+          onClick={() => deletePost(_id)}
+        ></i>
+      </div>
       <div className="card h-100">
         <img className="m-auto w-50" src={photo} alt="..." />
         <div className="card-body">
